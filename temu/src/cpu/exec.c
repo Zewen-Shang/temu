47ca155a684a5dea13d1112e91f4529e8f61eea9
uniqueke <247246702@qq.com>
2020-10-22 17:00:16 +0800

    加了add等

--- temu/src/cpu/exec.c
+++ temu/src/cpu/exec.c
@@ -29,24 +29,23 @@ op_fun opcode_table [64] = {
 /* 0x3c */	inv, inv, inv, inv
 };
 
-op_fun _2byte_opcode_table [64] = {
-/* 0x00 */	inv, inv, inv, inv, 
-/* 0x04 */	inv, inv, inv, inv, 
-/* 0x08 */	inv, inv, inv, inv, 
-/* 0x0c */	inv, inv, inv, inv, 
-/* 0x10 */	inv, inv, inv, inv, 
-/* 0x14 */	inv, inv, inv, inv, 
-/* 0x18 */	inv, inv, inv, inv, 
-/* 0x1c */	inv, inv, inv, inv, 
-/* 0x20 */	inv, inv, inv, inv, 
-/* 0x24 */	and, or,  xor, nor,
-/* 0x28 */	inv, inv, inv, inv, 
-/* 0x2c */	inv, inv, inv, inv, 
-/* 0x30 */	inv, inv, inv, inv, 
-/* 0x34 */	inv, inv, inv, inv,
-/* 0x38 */	inv, inv, inv, inv, 
-/* 0x3c */	inv, inv, inv, inv
-};
+op_fun _2byte_opcode_table[64] = {
+	/* 0x00 */ inv, inv, inv, inv,
+	/* 0x04 */ inv, inv, inv, inv,
+	/* 0x08 */ inv, inv, inv, inv,
+	/* 0x0c */ inv, inv, inv, inv,
+	/* 0x10 */ inv, inv, inv, inv,
+	/* 0x14 */ inv, inv, inv, inv,
+	/* 0x18 */ inv, inv, inv, inv,
+	/* 0x1c */ inv, inv, inv, inv,
+	/* 0x20 */ add, inv, inv, subu,
+	/* 0x24 */ and, or, xor, nor,
+	/* 0x28 */ inv, inv, slt, inv,
+	/* 0x2c */ inv, inv, inv, inv,
+	/* 0x30 */ inv, inv, inv, inv,
+	/* 0x34 */ inv, inv, inv, inv,
+	/* 0x38 */ inv, inv, inv, inv,
+	/* 0x3c */ inv, inv, inv, inv};
 
 make_helper(exec) {
 	instr = instr_fetch(pc, 4);
--- temu/src/cpu/r-type.c
+++ temu/src/cpu/r-type.c
@@ -20,6 +20,45 @@ static void decode_r_type(uint32_t instr) {
 	op_dest->reg = (instr & RD_MASK) >> (SHAMT_SIZE + FUNC_SIZE);
 }
 
+// make_helper(sll) {
+
+// 	decode_r_type(instr);
+// 	reg_w(op_dest->reg) = (op_src1->val + op_src2->val);
+// 	sprintf(assembly, "add   %s,   %s,   %s", REG_NAME(op_dest->reg), REG_NAME(op_src1->reg), REG_NAME(op_src2->reg));
+// }
+
+make_helper(add) {
+
+	decode_r_type(instr);
+	reg_w(op_dest->reg) = (op_src1->val + op_src2->val);
+	sprintf(assembly, "add   %s,   %s,   %s", REG_NAME(op_dest->reg), REG_NAME(op_src1->reg), REG_NAME(op_src2->reg));
+}
+
+make_helper(subu) {
+
+	decode_r_type(instr);
+	reg_w(op_dest->reg) = (op_src1->val - op_src2->val);
+	sprintf(assembly, "subu   %s,   %s,   %s", REG_NAME(op_dest->reg), REG_NAME(op_src1->reg), REG_NAME(op_src2->reg));
+}
+
+make_helper(slt) {
+
+	decode_r_type(instr);
+	// signed numbers compare
+	int rs = op_src1->val, rt = op_src2->val;
+	reg_w(op_dest->reg) = (rs < rt) ? 1 : 0;
+	sprintf(assembly, "slt   %s,   %s,   %s", REG_NAME(op_dest->reg), REG_NAME(op_src1->reg), REG_NAME(op_src2->reg));
+}
+
+// make_helper(mult) {
+
+// 	decode_r_type(instr);
+// 	// signed numbers multiple
+// 	int rs = op_src1->val, rt = op_src2->val;
+// 	reg_w(op_dest->reg) = (rs < rt) ? 1 : 0;
+// 	sprintf(assembly, "slt   %s,   %s,   %s", REG_NAME(op_dest->reg), REG_NAME(op_src1->reg), REG_NAME(op_src2->reg));
+// }
+
 make_helper(and) {
 
 	decode_r_type(instr);
